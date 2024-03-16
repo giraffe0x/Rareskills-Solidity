58338cd5e3e4d56c0623f4588f3d01457e91b6b9
Nicholas Chew <nicchew.eth@gmail.com>
2024-03-16 17:18:25 +0800

    Update solidity version

--- test/Week16/Synthetix/StakingRewardsOptimized.t.sol
+++ test/Week16/Synthetix/StakingRewardsOptimized.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.21;
 
 import { Test, console } from "forge-std/Test.sol";
 import {StakingRewardsOptimized} from "./../../../contracts/Week16/Synthetix/StakingRewardsOptimized.sol";
