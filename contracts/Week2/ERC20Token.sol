b0c47f62305258d2b9ab8fdaf04b089ffe5d4f63
Nicholas Chew <nicchew.eth@gmail.com>
2024-03-20 16:14:08 +0800

    Update

--- contracts/Week2/ERC20Token.sol
+++ contracts/Week2/ERC20Token.sol
@@ -8,9 +8,10 @@ pragma solidity 0.8.21;
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
-import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20Upgradeable.sol";
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract ERC20Token is ERC20Upgradeable, Ownable2StepUpgradeable, Initializable, UUPSUpgradeable {
+  /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() {
       _disableInitializers();
   }
@@ -18,6 +19,9 @@ contract ERC20Token is ERC20Upgradeable, Ownable2StepUpgradeable, Initializable,
   function initialize(string memory name_, string memory symbol_) public initializer {
       __ERC20_init(name_, symbol_);
       __Ownable2Step_init();
+      // __UUPSUpgradeable_init(); // Is this step needed doesn't seem to do anything
+
+      _mint(msg.sender, 1000000 * 10 ** decimals());
 
       approvedMinters[msg.sender] = 1;
       emit ApproveMinter(_minter);
