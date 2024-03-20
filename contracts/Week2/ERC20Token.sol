bb120e25d234ec47eaba368f99e79cd872679003
Nicholas Chew <nicchew.eth@gmail.com>
2024-03-20 15:48:41 +0800

    Adding upgradeability

--- contracts/Week2/ERC20Token.sol
+++ contracts/Week2/ERC20Token.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+// import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+// import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract ERC20Token is ERC20, Ownable2Step {
-  constructor() ERC20("ERC20Token", "ERC20") {}
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20Upgradeable.sol";
+
+contract ERC20Token is ERC20Upgradeable, Ownable2StepUpgradeable, Initializable, UUPSUpgradeable {
+  constructor() {
+      _disableInitializers();
+  }
+
+  function initialize(string memory name_, string memory symbol_) public initializer {
+      __ERC20_init(name_, symbol_);
+      __Ownable2Step_init();
+
+      approvedMinters[msg.sender] = 1;
+      emit ApproveMinter(_minter);
+  }
 
   mapping(address => uint256) public approvedMinters;
 
@@ -21,4 +37,6 @@ contract ERC20Token is ERC20, Ownable2Step {
 
     emit ApproveMinter(_minter);
   }
+
+  function _authorizeUpgrade(address) internal override onlyOwner {}
 }
